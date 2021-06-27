a49418ed06db790f4c63497bcca976d97ec65041
SATENDRAYADAV <43820135+SATENDRAYADAV@users.noreply.github.com>
2021-06-27 12:57:22 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.JS</h1>
+      <p1>Basic info about JS and React.JS and mini project</p1>
+    </div>
+  );
+}
+
+export default Info;
